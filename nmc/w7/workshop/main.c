b6d294169fce045299955812c08ad6d24214d84d
PS-Wizard <p.swoyam.1@gmail.com>
2025-01-07 08:49:22 +0545

    workshop 7 type shit lode png

---
+++ nmc/w7/workshop/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../lib/lodepng.h"
+
+int main() {
+    unsigned char *image;
+    unsigned width, height;
+    unsigned error;
+
+    error = lodepng_decode32_file(&image, &width, &height, "../wallpaper.png");
+    if (error) {
+        printf("Decode error %u: %s\n", error, lodepng_error_text(error));
+        return 1;
+    }
+
+    for (size_t i = 0; i < width * height * 4; i += 4) {
+        int grey = (image[i] + image[i+1] + image[i+2])/3;
+        image[i] = grey;
+        image[i + 1] = grey;
+        image[i + 2] = grey;
+    }
+
+    error = lodepng_encode32_file("output.png", image, width, height);
+    if (error) {
+        printf("Encode error %u: %s\n", error, lodepng_error_text(error));
+        free(image);
+        return 1;
+    }
+
+    free(image); 
+    return 0;
+}
