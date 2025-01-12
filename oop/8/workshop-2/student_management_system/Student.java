2060f360a1c5a0fb5286ea7fdf2b09a452c5c374
PS-Wizard <p.swoyam.1@gmail.com>
2025-01-12 12:14:30 +0545

    Added getter and setter method to studentclass

---
+++ oop/8/workshop-2/student_management_system/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    String name;
+    int age;
+    public String getName() {
+        return name; 
+    }
+    public int getAfge() {
+        return age; 
+    }
+    public String setName(String name) {
+        this.name = name;
+    }
+    public int setAfge(int age) {
+        this.age = age;
+    }
+
+}
+
