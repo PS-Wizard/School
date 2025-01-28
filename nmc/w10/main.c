81348ebd4ec17332f4141b490d9cee3b7c7bb2df
PS-Wizard <p.swoyam.1@gmail.com>
2025-01-28 14:40:54 +0545

    workshop 10

--- nmc/w10/main.c
+++ nmc/w10/main.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h> 
+#include <unistd.h>
 
-void *thread_function(void *arg) {
-    printf("Thread started. Doing work...\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Working... %d\n", i);
-        sleep(1); 
+void* threadOne(void* arg) {
+    for (int i = 1; i <= 5; i++) {
+        if (i == 3) {
+            printf("Thread 1 exiting at i = %d\n", i);
+            pthread_exit(NULL);  
+        }
+        printf("Thread 1: %d\n", i);
+        sleep(1);
     }
-    printf("Thread finished work.\n");
+    return NULL;
+}
+
+void* threadTwo(void* arg) {
+    for (int i = 1; i <= 5; i++) {
+        printf("Thread 2: %d\n", i);
+        pthread_testcancel();
+        sleep(1);
+    }
     return NULL;
 }
 
 int main() {
-    pthread_t thread;
+    pthread_t thread1, thread2;
 
-    if (pthread_create(&thread, NULL, thread_function, NULL) != 0) {
-        perror("Failed to create thread");
-        return 1;
-    }
+    pthread_create(&thread1, NULL, threadOne, NULL);
+    pthread_create(&thread2, NULL, threadTwo, NULL);
 
-    sleep(2);
+    sleep(3);  
 
-    printf("Main: Sending cancellation request to thread...\n");
-    if (pthread_cancel(thread) != 0) {
-        perror("Failed to cancel thread");
-        return 1;
-    }
+    printf("Canceling Thread 2...\n");
+    pthread_cancel(thread2);
 
-    if (pthread_join(thread, NULL) != 0) {
-        perror("Failed to join thread");
-        return 1;
-    }
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
-    printf("Main: Thread has been canceled.\n");
+    printf("Main thread finished.\n");
+
     return 0;
 }
