1a723cba5666ef430811b940d993661bc38cb188
PS-Wizard <p.swoyam.1@gmail.com>
2025-01-30 10:43:40 +0545

    i tried

--- oop/final-assesment/Main.java
+++ oop/final-assesment/Main.java
@@ -1,10 +1,7 @@
 import backend.db.DB_API;
 import backend.model.Game;
+import frontend.gui.PongGame;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Game.startGame();
-        System.out.println(Game.getScore());
-        DB_API.saveScore(69);
     }
 }
