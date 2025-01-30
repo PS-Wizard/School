520ae6870583b353d06501553c27a1cc466a4bad
PS-Wizard <p.swoyam.1@gmail.com>
2025-01-30 07:58:17 +0545

    OPENMP GO BRRR. Also finished task 3

--- nmc/final-assesment/T4/main.c
+++ nmc/final-assesment/T4/main.c
@@ -1,78 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../../lib/lodepng.h"
 
-void apply_gaussian_blur(int x, int y, unsigned char* image, unsigned char* output_image, int width, int height) {
-    int r_sum = 0, g_sum = 0, b_sum = 0;
-    int count = 0;
+typedef struct {
+    int dx, dy;
+} Neighbor;
 
-    // List of relative positions for the surrounding 8 pixels
-    int offsets[] = {-width-1, -width, -width+1, -1, 1, width-1, width, width+1};
+Neighbor neighbors[] = {
+    {-1, -1}, {0, -1}, {1, -1}, {-1, 0},{1, 0}, {-1, 1}, {0, 1}, {1, 1}
+};
 
-    for (int i = 0; i < 8; i++) {
-        int nx = x + offsets[i];
-        // Make sure it's within bounds
-        if (nx >= 0 && nx < width * height && (nx / width == (x + offsets[i]) / width)) {
-            r_sum += image[nx * 4];
-            g_sum += image[nx * 4 + 1];
-            b_sum += image[nx * 4 + 2];
-            count++;
+int is_valid_pixel(int x, int y, unsigned width, unsigned height) {
+    return x >= 0 && x < width && y >= 0 && y < height;
+}
+
+int pixel_index(int x, int y, unsigned width) {
+    return (y * width + x) * 4;
+}
+
+void apply_gaussian_blur(unsigned char* image, unsigned width, unsigned height) {
+    unsigned char* new_image = malloc(width * height * 4);
+    for (unsigned y = 0; y < height; y++) {
+        for (unsigned x = 0; x < width; x++) {
+            int r_sum = 0, g_sum = 0, b_sum = 0, count = 0;
+
+            for (int i = 0; i < 9; i++) {
+                int nx = x + neighbors[i].dx;
+                int ny = y + neighbors[i].dy;
+
+                if (is_valid_pixel(nx, ny, width, height)) {
+                    int idx = pixel_index(nx, ny, width);
+                    r_sum += image[idx];
+                    g_sum += image[idx + 1];
+                    b_sum += image[idx + 2];
+                    count++;
+                }
+            }
+
+            int new_idx = pixel_index(x, y, width);
+            new_image[new_idx] = r_sum / count;
+            new_image[new_idx + 1] = g_sum / count;
+            new_image[new_idx + 2] = b_sum / count;
+            new_image[new_idx + 3] = image[new_idx + 3]; 
         }
     }
-
-    // Add the current pixel itself
-    r_sum += image[x * 4];
-    g_sum += image[x * 4 + 1];
-    b_sum += image[x * 4 + 2];
-    count++;
-
-    // Set the blurred color to the output image
-    output_image[x * 4] = r_sum / count;
-    output_image[x * 4 + 1] = g_sum / count;
-    output_image[x * 4 + 2] = b_sum / count;
-    output_image[x * 4 + 3] = image[x * 4 + 3]; // Keep alpha as is
+    
+    memcpy(image, new_image, width * height * 4);
+    free(new_image);
 }
 
 int main() {
-    const char* input_filename = "./background.png";
-    const char* output_filename = "output.png";
+    const char* input = "./background.png";
+    const char* output = "./gaussian_blurred.png";
 
     unsigned char* image; 
-    unsigned width, height;
+    unsigned width, height; 
 
-    // Decode the image
-    unsigned error = lodepng_decode32_file(&image, &width, &height, input_filename);
+    unsigned error = lodepng_decode32_file(&image, &width, &height, input);
     if (error) {
         printf("Decoder error %u: %s\n", error, lodepng_error_text(error));
         return 1;
     }
+    apply_gaussian_blur(image, width, height);
 
-    printf("Decoded PNG: %ux%u pixels\n", width, height);
-
-    // Prepare output image (same size as input)
-    unsigned char* output_image = malloc(width * height * 4); // 4 bytes per pixel (RGBA)
-
-    // Apply Gaussian blur (excluding borders)
-    for (int y = 1; y < height - 1; y++) {
-        for (int x = 1; x < width - 1; x++) {
-            apply_gaussian_blur(x + y * width, y, image, output_image, width, height);
-        }
-    }
-
-    // Encode the image back to a PNG file
-    error = lodepng_encode32_file(output_filename, output_image, width, height);
+    error = lodepng_encode32_file(output, image, width, height);
     if (error) {
         printf("Encoder error %u: %s\n", error, lodepng_error_text(error));
-        free(image); 
-        free(output_image);
         return 1;
     }
 
-    printf("Saved PNG to %s\n", output_filename);
-
-    // Free allocated memory
     free(image);
-    free(output_image);
-
     return 0;
 }
