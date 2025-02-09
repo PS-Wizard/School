401b0adc28bc75e58db5d53323b3d6c0ecc79d7e
PS-Wizard <p.swoyam.1@gmail.com>
2025-02-09 09:47:47 +0545

    updated java snake

--- oop/final-assesment/Main.java
+++ oop/final-assesment/Main.java
@@ -1,7 +1,25 @@
-import backend.models.manager.*;
+import frontend.Snake;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        Manager.main(args);
+        Snake game = new Snake();
+
+        // Create a JFrame window to hold the game
+        JFrame frame = new JFrame("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.getContentPane().add(game);
+        frame.pack();
+        frame.setVisible(true);
+
+        // Timer to check for game over
+        Timer checkGameOverTimer = new Timer(100, e -> {
+            if (game.gameOver) {
+                frame.dispose();  // Close the window
+                ((Timer) e.getSource()).stop();  // Stop this timer
+            }
+        });
+        checkGameOverTimer.start();
     }
 }
--- oop/final-assesment/README.md
+++
@@ -1,7 +0,0 @@
-# Java Final Assesment
-![Dependencies](https://img.shields.io/badge/Dependes_on-MySql)
-![Dependencies](https://img.shields.io/badge/Dependes_on-MySql)
-
-## TODOS:
-- Create Snake Game lmao
-
